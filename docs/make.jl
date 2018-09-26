681f2c7dfae01ee8d3e10b8cc5f2e2a4164e0d90
Kinh <nguyenkinh@ytecongcong.com>
2018-09-27 00:52:30 +0200

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,18 +3,12 @@ using Documenter, Epi
 
 makedocs(
     modules = [Epi],
-    format = :html,
     sitename="Epi.jl",
     pages = ["Home" => "index.md"]
 )
 
 deploydocs(
     repo = "github.com/kklot/Epi.jl.git",
-    target = "build",
-    branch = "gh-pages",
-    latest = "master",
-    deps = nothing,
-    make = nothing,
     julia = "1.0",
-    osname = "osx"
+    osname = "linux"
 )
